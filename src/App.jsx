4b4e26d6074f504f139984adb5846a505d6c6fcf
PSYZANE <adityabudakoti3333@gmail.com>
2025-05-04 23:19:39 +0530

    made navbar
    navbar done, hero section next

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import Navbar from './components/navbar'
 import Hero from './components/hero'
 
 const App = () => {
   return (
-    <div>
+    <div className='container mx-auto px-0.5 sm:px-3 md:px-5'>
       <Navbar/>
       <Hero/>
     </div>
